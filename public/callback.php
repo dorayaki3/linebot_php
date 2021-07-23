085477ff009df2f0fb07759f37fad3c8341af1dd
dorayaki3 <56065665+dorayaki3@users.noreply.github.com>
2021-07-23 20:31:07 +0900

    Update callback.php

--- public/callback.php
+++ public/callback.php
@@ -1,29 +1,25 @@
 <?php
  
-$accessToken = 'IPLZVNBSk++TdfiVDGm1eWXBNI45nlOQwUFwwpHa91Co25o9LiB/utwiQeO4ZRwea7AJpF3jlc7Od54XT1dN5DDtc91VBJw2n0TyRqQ6GHXQTf0gu5xUGdFX+k1r/a4iGyFlJMp2RDchtmTpcMmP9QdB04t89/1O/w1cDnyilFU=';
-
+$accessToken = 'ここに「Channel Access Token」をコピペする';
+ 
+//ユーザーからのメッセージ取得
 $json_string = file_get_contents('php://input');
 $json_object = json_decode($json_string);
-
+ 
+//取得データ
 $replyToken = $json_object->{"events"}[0]->{"replyToken"};        //返信用トークン
 $message_type = $json_object->{"events"}[0]->{"message"}->{"type"};    //メッセージタイプ
 $message_text = $json_object->{"events"}[0]->{"message"}->{"text"};    //メッセージ内容
  
+//メッセージタイプが「text」以外のときは何も返さず終了
+if($message_type != "text") exit;
+ 
 //返信メッセージ
-$return_message_text = "8月中にはなにかしゃべらせたい。以降はメモ。herokuでコードはgithub" ;
-$now =  $objDateTime->format('Y-m-d H:i:s') ;
-
-if($text == "\neko"){
-    $neko = "にゃーん";
-    sending_messages($accessToken, $replyToken, $message_type, $neko);
-}
-
-else{
+$return_message_text = "「" . $message_text . "」じゃねーよｗｗｗ";
+ 
+//返信実行
 sending_messages($accessToken, $replyToken, $message_type, $return_message_text);
-sending_messages($accessToken, $replyToken, $message_type, $now);
-}
 ?>
-
 <?php
 //メッセージの送信
 function sending_messages($accessToken, $replyToken, $message_type, $return_message_text){
